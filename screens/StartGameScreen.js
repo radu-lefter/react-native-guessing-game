4d9713c3db789c1225b6e8ea4b0540b596b81735
raduubraduu <lefter_radu@yahoo.com>
2023-03-20 21:22:47 +0000

    implemented functionlity to change screens on input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import { TextInput, View, StyleSheet, Alert } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+function StartGameScreen({onPickNumber}) {
 
   const [enteredNumber, setEnteredNumber] = useState('');
 
@@ -27,7 +27,7 @@ function StartGameScreen() {
       return;
     }
 
-    console.log('Valid number!');
+    onPickNumber(chosenNumber);
   }
 
   return (

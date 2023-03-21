df58b400345c090e812cbc38546c0aee9037a938
raduubraduu <lefter_radu@yahoo.com>
2023-03-21 18:26:13 +0000

    adjusted screen size dynamically ith the useWindowDimensions hook in start game screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { TextInput, View, StyleSheet, Alert } from "react-native";
+import { TextInput, View, StyleSheet, Alert, useWindowDimensions, } from "react-native";
 
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Colors from '../constants/colors';
@@ -10,6 +10,8 @@ import InstructionText from '../components/ui/InstructionText';
 function StartGameScreen({onPickNumber}) {
 
   const [enteredNumber, setEnteredNumber] = useState('');
+
+  const { width, height } = useWindowDimensions();
 
   function numberInputHandler(enteredText) {
     setEnteredNumber(enteredText);
@@ -34,8 +36,10 @@ function StartGameScreen({onPickNumber}) {
     onPickNumber(chosenNumber);
   }
 
+  const marginTopDistance = height < 380 ? 30 : 100;
+
   return (
-    <View style={styles.rootContainer}>
+    <View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
       <Title>Guess My Number</Title>
       <Card>
         <InstructionText>

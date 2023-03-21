e71f2fde6be3b1e506ed0430cea9e98a444c6d77
raduubraduu <lefter_radu@yahoo.com>
2023-03-21 19:05:00 +0000

    targeted title component for different platforms

--- components/ui/Title.android.js
+++ components/ui/Title.android.js
@@ -1,5 +1,4 @@
-import { Text, StyleSheet } from 'react-native';
-
+import { Text, StyleSheet, Platform } from 'react-native';
 
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -14,10 +13,12 @@ const styles = StyleSheet.create({
     // fontWeight: 'bold',
     color: 'white',
     textAlign: 'center',
+    // borderWidth: Platform.OS === 'android' ? 2 : 0,
+    // borderWidth: Platform.select({ ios: 0, android: 2 }),
     borderWidth: 2,
     borderColor: 'white',
     padding: 12,
     maxWidth: '80%',
-    width: 300
+    width: 300,
   },
 });
